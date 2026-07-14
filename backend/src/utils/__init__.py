f524d61b9020fb375b90ed3216c8b1de147a831b
yinlchen <yinlchen@yinlchens-MacBook-Pro.local>
2026-07-14 14:50:36 +0800

    chore: remove unused backend/src and frontend_wireframe - backend/src: replaced by sls-api/handler.py (Lambda) - frontend_wireframe: replaced by frontend/

--- backend/src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-工具模組
-"""
-from .config import settings
-from .logger import setup_logger
-from .s3_client import s3_client
-from .dynamodb_client import dynamodb_client
-
-__all__ = ['settings', 'setup_logger', 's3_client', 'dynamodb_client']
